1ec61697ce0b11d945db6b90e0a62f86b3272143
Claudia-20 <claudia.movradin@gmail.com>
2020-08-21 14:52:02 +0300

    Added first task

---
+++ Claudia/src/main/java/com/endava/task/first/Numbers.java
@@ -0,0 +1,10 @@
+package com.endava.task.first;
+
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println("Result is : ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
