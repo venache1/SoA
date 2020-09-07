40d62411bbad50517f018327b3023ef8b5b38fe4
ipanaiotova <irina.panaiotova@endava.com>
2020-09-07 13:49:12 +0300

    [Artiom]Collection task

---
+++ Artiom/src/main/java/Item.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Item {
+    public long id;
+    public String title;
+    public Type type;
+
+    public Item(String title, Type type) {
+        this.id = new Random().nextLong();
+        this.title = title;
+        this.type = type;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
+
---
+++ Irina/src/main/java/firsttasks/DisplayNumberUsingFor.java
@@ -0,0 +1,12 @@
+package firsttasks;
+/*
+Display numbers from 1 to 10 using for()
+ */
+
+public class DisplayNumberUsingFor {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 10; number++) {
+            System.out.println(number);
+        }
+    }
+}
